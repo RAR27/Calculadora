b1a3786154f9eb887a548ccff11e3d9fc6a14ea4
RAR27 <romulo.antonio_rodrigues@hotmail.com>
2024-08-06 13:16:56 -0300

    Versão calculadora

---
+++ Calc/src/main/java/org/example/Func.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Func {
+
+    public int valorSoma(int n1, int n2){
+        return n1 + n2;
+    }
+
+
+    public int valorSubtracao(int n1, int n2){
+        return n1 - n2;
+    }
+
+
+    public int valorDivisao(int n1, int n2){
+        return n1 / n2;
+    }
+
+
+    public int valorMultiplicacao(int n1, int n2){
+        return n1 * n2;
+    }
+}
---
+++ Calc/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1;
+        int n2;
+        int resultado;
+
+        Scanner ler = new Scanner(System.in);
+        Func calculadora = new Func();
+
+        System.out.println("Digite o primeiro número: ");
+        n1 = ler.nextInt();
+        System.out.println("Digite o segundo número: ");
+        n2 = ler.nextInt();
+
+        resultado = calculadora.valorSoma(n1, n2);
+        System.out.println("O resultado da soma é: " + resultado);
+
+        resultado = calculadora.valorSubtracao(n1, n2);
+        System.out.println("O resultado da subtração é: " + resultado);
+
+        resultado = calculadora.valorDivisao(n1, n2);
+        System.out.println("O resultado da divisão é: " + resultado);
+
+        resultado = calculadora.valorMultiplicacao(n1, n2);
+        System.out.println("O resultado da multiplicação é: " + resultado);
+    }
+}
